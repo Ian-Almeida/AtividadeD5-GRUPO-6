ac0962432a2d49135757195851cc976c2821c0c4
Ian Almeida <ian.almeida@edu.univali.br>
2022-08-10 14:33:59 -0300

    Criadas as telas de Login e Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,71 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="container" style="height: 200px; width: 200px; padding: 10px;">
+      <div class="img-logo"> </div>
+    </div>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light p-2">
+      <a class="navbar-brand" href="#">Wxyz</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Grade Horária</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Notas
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mensagens</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Financeiro</a>
+          </li>
+        </ul>
+        
+      </div>
+      
+      <div class="dropstart">
+        <button class="btn btn-text" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <span style="font-size: 16pt">. . .</span>
+        </button>
+        <ul class="dropdown-menu">
+          <li><router-link class="dropdown-item" :to="{name: 'main-home-about'}">Sobre</router-link></li>
+          <li><a class="dropdown-item pointer btn" @click="logout">Sair</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view></router-view>
+    
+    
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+const router = useRouter();
+
+function logout() {
+  localStorage.loggedIn = 'false';
+  router.push({name: 'main-login'});
+}
+
 </script>
+
+<style scoped>
+.img-logo {
+  width: 200px;
+  height: 200px;
+  background-color: red;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-view/>
+</template>
+
+<script setup lang="ts">
+
+import {onBeforeMount} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+
+onBeforeMount(() => {
+  if (localStorage?.loggedIn) {
+    if (localStorage.loggedIn === 'false') router.push({name: 'main-login'});
+    else if (!router.currentRoute.value.fullPath.includes('/home')) router.push({name: 'main-home'});
+  } else {
+    router.push({path: '/login'});
+  }
+
+})
+</script>
+
+<style scoped>
+
+</style>
